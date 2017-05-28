6c082f778f31c2bfdf30a4b607e3e6d553dbf5f9
Armin Haskovic <armin.haskovic@gmail.com>
2017-05-28 12:07:27 +0000

    mapper and reducer used for tempo now

--- mapper.py
+++ mapper.py
@@ -1,7 +1,30 @@
-import hdf5_getter
+#!/usr/bin/python3i
 
-h5 = hdf5_getter.open_h5_file_read("/home/adsc6129/Desktop/LDSA_Project/TRAXLZU12903D05F94.h5")
-duration = hdf5_getter.get_duration(h5)
-print duration
-print(hdf5_getter.get_artist_name(h5))
-h5.close()
+import sys
+
+for line in sys.stdin:
+        line = line.strip()
+        line = line.split(',')
+
+        #SongId = line[0]
+        #AlbumID = line[1]
+        #AlbumName = line[2]
+        #ArtistID = line[3]
+	#ArtistLatitude = line[4]
+        #ArtistLocation = line[5]
+        #ArtistLongitude = line[6]
+        #ArtistName = line[7]
+        #danceability = float(line[8])
+        #Duration = line[9]
+        #KeySignature = line[10]
+        #KeySignatureConfidence = line[11]
+        tempo = float(line[12])
+        #TimeSignature = line[13]
+        #TimeSignatureConfidence = line[14]
+        #Title = line[15]
+        year = int(line[16])
+        hotness = float(line[17])
+        #print(tempo)
+        if hotness > 0.5:
+                if (year > 0 and tempo > 0):
+                    print('%s \t %s' % (year, tempo))
